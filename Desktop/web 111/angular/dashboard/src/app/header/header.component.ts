8df77c43f455098af59f6c570de75296d3680b20
Amalvm007 <amalvm@73gamil.com>
2023-01-03 11:36:54 +0530

    dashboard creation

---
+++ Desktop/web 111/angular/dashboard/src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { outputAst } from '@angular/compiler';
+import { Component,EventEmitter,Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  @Output() isSidebarToggle=new EventEmitter()
+
+
+
+  sideBarToggle(){
+    this.isSidebarToggle.emit()
+    setTimeout(()=>{
+      window.dispatchEvent(
+        new Event('resize')
+      )
+    },300)
+
+  }
+
+}
